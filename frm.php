c11375d3b2e1ee802f3b9c6cecc4bcd07faaace1
rashed <92414937+mrashidulcom@users.noreply.github.com>
2024-08-17 17:21:37 +0600

    update all repeteted file

--- frm.php
+++ frm.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,51 +20,3 @@
     </div>
 </body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div>
-    <h2>Simple Form</h2>
-    <form action="frm-submit.php" method="post">
-        <label for="text">Text:</label><br>
-        <input type="text" id="text" name="text" required><br><br>
-        <input type="submit" value="Submit">
-    </form>
-
-    <h2>Entries</h2>
-    <?php include 'display.php'; ?>
-    </div>
-</body>
-</html>
->>>>>>> ebd8675763b0223774f4c8821a30c05b1ecd60f0
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div>
-    <h2>Simple Form</h2>
-    <form action="frm-submit.php" method="post">
-        <label for="text">Text:</label><br>
-        <input type="text" id="text" name="text" required><br><br>
-        <input type="submit" value="Submit">
-    </form>
-
-    <h2>Entries</h2>
-    <?php include 'display.php'; ?>
-    </div>
-</body>
-</html>
->>>>>>> ebd8675763b0223774f4c8821a30c05b1ecd60f0
